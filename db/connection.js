519baf0006a298ed40701684f49add88b38ac319
giorgiadifranco <giorgiadf08@gmail.com>
2024-12-06 15:27:54 +0100

    fix connection

--- db/connection.js
+++ db/connection.js
@@ -4,6 +4,14 @@ const connection = mysql.createConnection({
 
     /*inserire host, user, password e database --> conservate in .env*/
 
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT
+
+
+
 })
 
 //gestione errore
@@ -12,3 +20,4 @@ connection.connect(err =>{
     console.log('connected to my sql')
 
 })
+module.exports = connection;
